b006b6d4c4ef6837a38d1f3ba1ccf077540a93a5
Wolfetti <wolfetti.bsk@gmail.com>
2026-01-20 22:19:39 +0100

    Fix java.lang.ClassNotFoundException: com.jcraft.jsch.jce.Random

--- src/main/java/org/github/wolfetti/ssh/conf/ReflectionConfiguration.java
+++ src/main/java/org/github/wolfetti/ssh/conf/ReflectionConfiguration.java
@@ -2,13 +2,34 @@ package org.github.wolfetti.ssh.conf;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(targets = {
-    com.jcraft.jsch.JSch.class,
-    com.jcraft.jsch.Session.class,
-    com.jcraft.jsch.Channel.class,
-    com.jcraft.jsch.ChannelDirectTCPIP.class,
-    com.jcraft.jsch.KeyPair.class
-}, registerFullHierarchy = true)
+@RegisterForReflection(
+    registerFullHierarchy = true,
+    classNames = {
+        "com.jcraft.jsch.JSch",
+        "com.jcraft.jsch.Session",
+        "com.jcraft.jsch.Channel",
+        "com.jcraft.jsch.ChannelDirectTCPIP",
+        "com.jcraft.jsch.KeyPair",
+        "com.jcraft.jsch.jce.Random",
+        "com.jcraft.jsch.jce.KeyPairGenRSA",
+        "com.jcraft.jsch.jce.KeyPairGenDSA",
+        "com.jcraft.jsch.jce.KeyPairGenECDSA",
+        "com.jcraft.jsch.jce.SignatureRSA",
+        "com.jcraft.jsch.jce.SignatureDSA",
+        "com.jcraft.jsch.jce.SignatureECDSA",
+        "com.jcraft.jsch.jce.HMACSHA1",
+        "com.jcraft.jsch.jce.AES128CBC",
+        "com.jcraft.jsch.jce.AES128CTR",
+        "com.jcraft.jsch.jce.BlowfishCBC",
+        "com.jcraft.jsch.jce.TripleDESCBC",
+        "com.jcraft.jsch.jce.PBKDF2",
+        "com.jcraft.jsch.jce.ECDH256",
+        "com.jcraft.jsch.jce.ECDH384",
+        "com.jcraft.jsch.jce.ECDH521",
+        "com.jcraft.jsch.jgss.GSSContextKrb5",
+        "com.jcraft.jsch.jcraft.Compression"
+    }
+)
 public class ReflectionConfiguration {
 
 }
